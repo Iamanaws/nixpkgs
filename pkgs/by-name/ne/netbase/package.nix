226e8082bceee204aa521013bd66dfc79361fa4c
Angel J <78835633+iamanaws@users.noreply.github.com>
2026-02-21 10:56:44 -0800

    netbase: 0.1 -> 0.1-unstable-2026-02-21

--- pkgs/by-name/ne/netbase/package.nix
+++ pkgs/by-name/ne/netbase/package.nix
@@ -2,38 +2,45 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  byacc,
   clang,
   llvm,
   gnumake,
+  acl,
   bzip2,
   zlib,
+  musl-fts,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "netbase";
-  version = "0.1";
+  version = "0.1-unstable-2026-02-21";
 
   src = fetchFromGitHub {
     owner = "littlefly365";
     repo = "Netbase";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-o/O3pqdYY3Q/ut7P7sbYaivlWJuwhhGmMrMBSPbslV4=";
+    rev = "4e4acc839fc31f2f2ea9ca68e875773b43eccd9c";
+    hash = "sha256-tLdHEzCdH6MPe6eBXTzLDLg25WdBPU8eRW+HrSd/m9c=";
   };
 
   strictDeps = true;
 
   nativeBuildInputs = [
-    byacc
     clang
     llvm
     gnumake
   ];
 
   buildInputs = [
+    acl
     bzip2
     zlib
-  ];
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isMusl [ musl-fts ];
+
+  env = lib.optionalAttrs stdenv.hostPlatform.isMusl {
+    NIX_CFLAGS_COMPILE = "-I${musl-fts}/include";
+    NIX_LDFLAGS = "-L${musl-fts}/lib";
+  };
 
   postPatch = ''
     substituteInPlace bin/ksh/Makefile \
@@ -42,6 +49,18 @@ stdenv.mkDerivation (finalAttrs: {
 
   buildPhase = ''
     runHook preBuild
+    [ -f scripts/linux/extattr.h ] && cp scripts/linux/extattr.h include/sys/
+  ''
+  + lib.optionalString stdenv.hostPlatform.isMusl ''
+    mkdir -p include/protocols include/sys
+
+    [ -f scripts/musl/cdefs.h ] && cp scripts/musl/cdefs.h include/sys/
+    [ -f scripts/musl/queue.h ] && cp scripts/musl/queue.h include/sys/
+    [ -f scripts/musl/timed.h ] && cp scripts/musl/timed.h include/protocols/
+
+    export LDFTS="-lfts"
+  ''
+  + ''
     make -f GNUmakefile -j$NIX_BUILD_CORES
     runHook postBuild
   '';
